139a0a2b83aaed1972883a673ea1df60cefb6d75
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2023-11-13 12:04:12 +0530

    add more files
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

---
+++ navigation_guide.sh
@@ -0,0 +1,11 @@
+gg - move the begining of the file
+G - end of the file
+
+^ - to start of the line
+$ to the end of the line
+
+
+ctrl + d - scroll down half a page
+ctrl + u : scroll up half a page
+
+
