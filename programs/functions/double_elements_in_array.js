fc79a285e987c9859470c66dce33d7564a02be8e
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2023-11-25 23:58:08 +0530

    update
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

---
+++ programs/functions/double_elements_in_array.js
@@ -0,0 +1,5 @@
+function doubleA(arr) {
+    return arr.map((item) => item * 2)
+}
+
+console.log(doubleA([1, 2, 3]))
--- programs/question.go
+++ programs/question.go
@@ -1,1 +1,29 @@
-package programs
+package main
+
+import "fmt"
+
+type Student struct {
+	name string
+}
+
+func printChangedName(student *Student) {
+	name1 := student.name
+	fmt.Println(name1)
+}
+
+func printString(s *string) {
+	*s = "world"
+	fmt.Println(*s, s)
+}
+func main() {
+	s := "hello"
+	printString(&s)
+
+	student := Student{"Vivek"}
+	printChangedName(&student)
+}
+
+// question
+/*
+why
+*/
