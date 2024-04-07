a834e0583f81f86a1bc9fb61a0beb1d74cea60d0
Vivek Vishal <vishalvivek488@gmail.com>
2024-04-07 23:57:26 +0530

    initial commit

---
+++ go-redis/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func main() {
+	log.Info("starting server..")
+	http.HandleFunc("/api", Handler)
+
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
+
+func Handler(writer http.ResponseWriter, request *http.Request) {
+	log.Info("from server")
+
+	resp := APIReponse{
+		Cache: false,
+		Data:  make([]NominatimResponse, 0),
+	}
+	err := json.NewEncoder(writer).Encode(resp)
+	if err != nil {
+		log.Fatalf("error encoding the response: %v", err)
+		writer.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+type APIReponse struct {
+	Cache bool                `json:"cache"`
+	Data  []NominatimResponse `json:"data"`
+}
+type NominatimResponse struct {
+	PlaceID     int      `json:"place_id"`
+	Licence     string   `json:"licence"`
+	OsmType     string   `json:"osm_type"`
+	OsmID       int      `json:"osm_id"`
+	Lat         string   `json:"lat"`
+	Lon         string   `json:"lon"`
+	Class       string   `json:"class"`
+	Type        string   `json:"type"`
+	PlaceRank   int      `json:"place_rank"`
+	Importance  float64  `json:"importance"`
+	Addresstype string   `json:"addresstype"`
+	Name        string   `json:"name"`
+	DisplayName string   `json:"display_name"`
+	Boundingbox []string `json:"boundingbox"`
+}
+
